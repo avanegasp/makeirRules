4b926f69972ac19dcbffa42146cb1a79cf7d1118
avanegasp <arco527@gmail.com>
2017-08-21 20:57:16 -0500

    Only checkbox is not ready

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
-
 $('form').submit(function(e){
   e.preventDefault();
+  $('#first-name').css('border-color', '')
+  $('#last-name').css('border-color', '')
+  $('#mobile-number').css('border-color', '')
+  $('input:checkbox').removeAttr('checked'); 
 
   var nombre = $('#first-name').val();
   var apellido = $('#last-name').val();
   var celular = $('#mobile-number').val();
+  var confirmar = $('#confirmed').val();
 
     if(nombre.length > 0){
       if(apellido.length > 0){
         if(celular.length === 10 && !isNaN(celular)){
             if($('#confirmed').is(':checked')){
-                console.log('Todo esta bien, agrega el registro')
             } else{
               $('#confirmed').css('border-color','#FF0000');
               return false;
@@ -23,17 +26,20 @@ $('form').submit(function(e){
         $('#last-name').css('border-color','#FF0000');
         return false;
       }
-    }else if ($('#first-name').css('border-color','#FF0000')){
-      return false;
     }else{
-      $('#first-name').css('','');
+      $('#first-name').css('border-color','#FF0000');
+      return false;
     }
-
 
   $('.table tbody').append('<tr>' + '<td>' + nombre + '</td><td>' + apellido + '</td><td>' + celular + '</td><td>' + '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'  + '</td></tr>')
 
   $('#first-name').val('');
   $('#last-name').val('');
   $('#mobile-number').val('');
+  $('#confirmed').val('');
+
+
+  $('#new-guest').modal('hide');
+
 
 });
